9b4eaadf5e7ae0545c7aba7f552238bd1e2b620f
SANVI <sanvijsr1@gmail.com>
2026-01-13 20:14:10 +0530

    zod application

--- mongotrial.js
+++ mongotrial.js
@@ -1,29 +1,52 @@
 const express = require('express');
 const app = express()
+const zod = require("zod")
 const port = 3400
-app.get('/health-checkup', function(req,res){
-    const kidneyId = req.query.kidneyId
-    const username = req.headers.username
-    const password = req.headers.password
 
-    if(username!='Sanvi' || password!='pass'){
-        res.status(403).json({
-            msg: "User doesnot exist"
-        })
-        return;
-    }
+const schema = zod.array(zod.number())
 
-    if(kidneyId!=1 && kidneyId!=2){
+app.use(express.json())
+
+app.post("/health-checkup", function(req, res){
+    //kidneys=[1,2,3,4]
+    const kidneys = res.body.kidneys;
+    const response = schema.safeParse(kidneys)
+    if(!response.success){
         res.status(411).json({
-            msg: "wrong inputs"
+            msg:" input is invalid"
         })
-        return;
     }
-
-    res.json({
-        msg: "ur kidney is fine"
+    else{
+        res.send({
+        response
     })
+    }
+    
 })
+
+// app.get('/health-checkup', function(req,res){
+//     const kidneyId = req.query.kidneyId
+//     const username = req.headers.username
+//     const password = req.headers.password
+
+//     if(username!='Sanvi' || password!='pass'){
+//         res.status(403).json({
+//             msg: "User doesnot exist"
+//         })
+//         return;
+//     }
+
+//     if(kidneyId!=1 && kidneyId!=2){
+//         res.status(411).json({
+//             msg: "wrong inputs"
+//         })
+//         return;
+//     }
+
+//     res.json({
+//         msg: "ur kidney is fine"
+//     })
+// })
 
 app.listen(port, ()=>{
     console.log("chal rha h")
