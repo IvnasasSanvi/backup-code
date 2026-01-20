4c0ead88a8a576c58067b0b5aa25fb4b591844e4
SANVI <sanvijsr1@gmail.com>
2026-01-20 19:59:11 +0530

    use of mongoose

--- usemongo.js
+++ usemongo.js
@@ -1,7 +1,65 @@
-import mongoose  from 'mongoose';
-mongoose.connect('mongodb://mongodb+srv://sanvijsr1_db_user:Sanvijsr16@cluster0.px5mkid.mongodb.net/suume');
+// import mongoose from "mongoose";
 
-const Cat = mongoose.model('Cat', { name: String });
+// mongoose.connect(
+//   "mongodb+srv://sanvijsr1_db_user:Sanvijsr16@cluster0.px5mkid.mongodb.net/sanvi?retryWrites=true&w=majority"
+// )
+// .then(() => console.log("MongoDB connected"))
+// .catch(err => console.error(err.message));
 
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+// const Cat = mongoose.model("Cat", { name: String });
+
+// const kitty = new Cat({ name: "Zildjian" });
+// kitty.save().then(() => console.log("meow"));
+
+
+// import mongoose from "mongoose";
+
+// async function main() {
+//   try {
+//     await mongoose.connect(
+//   "mongodb+srv://sanvijsr1_db_user:Sanvijsr16@cluster0.px5mkid.mongodb.net/user?retryWrites=true&w=majority"
+// );
+
+
+//     console.log("MongoDB connected");
+
+//     const Cat = mongoose.model("Cat", { name: String });
+
+//     const kitty = new Cat({ name: "Zildjian" });
+//     await kitty.save();
+
+//     console.log("meow");
+//     process.exit(0);
+
+//   } catch (err) {
+//     console.error("Mongo error:", err.message);
+//   }
+// }
+
+// main();
+
+
+import mongoose from "mongoose";
+
+async function main() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://sanvijsr1_db_user:Sanvijsr16@cluster0.px5mkid.mongodb.net/sanvi?retryWrites=true&w=majority"
+    );
+
+    console.log("MongoDB connected");
+
+    const Cat = mongoose.model("Cat", { name: String });
+
+    const kitty = new Cat({ name: "Zildjian" });
+    await kitty.save();
+
+    console.log("meow");
+    await mongoose.disconnect();
+
+  } catch (err) {
+    console.error("Mongo error:", err.message);
+  }
+}
+
+main();
