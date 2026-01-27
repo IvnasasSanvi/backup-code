92c1b9b94398ae1d77c2f66e973f6a03c7f9c187
SANVI <sanvijsr1@gmail.com>
2026-01-27 23:09:39 +0530

    react pratice

--- react/reactproject/src/App.jsx
+++ react/reactproject/src/App.jsx
@@ -1,24 +1,36 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 // default normal
 
-const App = () => {
-  const [count, setcount]= useState(0)
+// const App = () => {
+//   const [count, setcount]= useState(0)
   
-  return (
-    <div>
-      <CustomButton count = {count} setcount={setcount}></CustomButton>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <CustomButton count = {count} setcount={setcount}></CustomButton>
+//     </div>
+//   )
+// }
 
 //component
-function CustomButton(props){
-  function onClickHandler(){
-    props.setcount(props.count + 1)
-  }
-  return(
-    <button onClick={onClickHandler}>count = {props.count}</button>
-  )
+// function CustomButton(props){
+//   function onClickHandler(){
+//     props.setcount(props.count + 1)
+//   }
+//   return(
+//     <button onClick={onClickHandler}>count = {props.count}</button>
+//   )
+// }
+
+function App() {
+  const [todos, seTodos] = useState([{
+    title: "go to gym",
+    description: "workout at 7am",
+    completed: false
+  },{
+    title: "go to gym",
+    description: "workout at 10am",
+    completed: true
+  }])
 }
 
 export default App
