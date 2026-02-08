d68d0642e095b771e80e06e2e20bec66e99b13c2
SANVI <sanvijsr1@gmail.com>
2026-02-08 23:18:26 +0530

    react practice

--- react/reactproject/src/App.jsx
+++ react/reactproject/src/App.jsx
@@ -54,16 +54,48 @@ import Button from '../component/Button'
 //   )
 // }
 
-function App(){
-  const [count,setcount] = useState(0)
-  return(
-    <>  
-    <Button count={count} setname={setcount} ></Button>
-    <Button count={count} setname={setcount}></Button>
-    <Button count={count} setname={setcount}></Button>
-    <Button count={count} setname={setcount}></Button>
-    </>
+
+//this attach Button.jsx with the app file
+// function App(){
+//   const [count,setcount] = useState(0)
+//   return(
+//     <>  
+//     <Button count={count} setname={setcount} ></Button>
+//     <Button count={count} setname={setcount}></Button>
+//     <Button count={count} setname={setcount}></Button>
+//     <Button count={count} setname={setcount}></Button>
+//     </>
+//   )
+// }
+
+
+function App() {
+  return (
+    <div>
+      <HeaderWithButton />
+      <Header title="My name is raman" />
+    </div>
   )
 }
+
+function HeaderWithButton() {
+  const [firstTitle, setFirstTitle] = useState("my name is harkirat");
+
+  function changeTitle() {
+    setFirstTitle("My name is " + Math.random())
+  }
+
+  return <>
+    <button onClick={changeTitle}>Click me to change the title</button>
+    <Header title={firstTitle} />
+  </>
+}
+
+function Header({title}) {
+  return <div>
+    {title}
+  </div>
+}
+
 
 export default App
