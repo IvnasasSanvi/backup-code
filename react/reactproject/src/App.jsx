b9f6c29c61b3823cf7beb769908651c9811ff33f
SANVI <sanvijsr1@gmail.com>
2026-02-13 23:17:07 +0530

    react practice

--- react/reactproject/src/App.jsx
+++ react/reactproject/src/App.jsx
@@ -215,35 +215,35 @@ import NavBar from '../component/NavBar'
 //   )
 // }
 
-import { useState } from "react";
-import { useEffect } from "react";
+// import { useState } from "react";
+// import { useEffect } from "react";
 
-function App() {
-  const [todos, setTodos] = useState([])
+// function App() {
+//   const [todos, setTodos] = useState([])
 
-  useEffect(() => {
-    fetch("https://sum-server.100xdevs.com/todos")
-      .then(async function(res) {
-        const json = await res.json();
-        setTodos(json.todos);
-      })
-  }, [])
+//   useEffect(() => {
+//     fetch("https://sum-server.100xdevs.com/todos")
+//       .then(async function(res) {
+//         const json = await res.json();
+//         setTodos(json.todos);
+//       })
+//   }, [])
 
-  return <div>
-    {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
-  </div>
-}
+//   return <div>
+//     {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+//   </div>
+// }
 
-function Todo({title, description}) {
-  return <div>
-    <h1>
-      {title}
-    </h1>
-    <h4>
-      {description}
-    </h4>
-  </div>
-}
+// function Todo({title, description}) {
+//   return <div>
+//     <h1>
+//       {title}
+//     </h1>
+//     <h4>
+//       {description}
+//     </h4>
+//   </div>
+// }
 
 //useEffect
 //import { useState } from "react";
