aa5587be033deb5b55c0388e758251ceb738adae
SANVI <sanvijsr1@gmail.com>
2026-02-14 23:16:45 +0530

    react practice

--- react/reactproject/src/App.jsx
+++ react/reactproject/src/App.jsx
@@ -249,31 +249,31 @@ import NavBar from '../component/NavBar'
 //import { useState } from "react";
 //import { useEffect } from "react";
 
-function App() {
-  return <div>
-    <Todo id={1} />
-  </div>
-}
+// function App() {
+//   return <div>
+//     <Todo id={1} />
+//   </div>
+// }
 
-function Todo({id}) {
-  const [todo, setTodo] = useState({});
+// function Todo({id}) {
+//   const [todo, setTodo] = useState({});
 
-  useEffect(() => {
-    fetch("https://sum-server.100xdevs.com/todo?id=" + id)
-      .then(async function(res) {
-        const json = await res.json();
-        setTodo (json.todo);
-      })
-  }, [])
+//   useEffect(() => {
+//     fetch("https://sum-server.100xdevs.com/todo?id=" + id)
+//       .then(async function(res) {
+//         const json = await res.json();
+//         setTodo (json.todo);
+//       })
+//   }, [])
 
-  return <div>
-    <h1>
-      {todo.title}
-    </h1>
-    <h4>
-      {todo.description}
-    </h4>
-  </div>
-}
+//   return <div>
+//     <h1>
+//       {todo.title}
+//     </h1>
+//     <h4>
+//       {todo.description}
+//     </h4>
+//   </div>
+// }
 
 export default App
