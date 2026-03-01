ed7b674c3726424221bd31f8e2ad9233398f9608
SANVI <sanvijsr1@gmail.com>
2026-03-01 22:25:52 +0530

    postgres

--- mainFolder/postgrestrial.js
+++ mainFolder/postgrestrial.js
@@ -6,4 +6,18 @@ const postgrestrial = () => {
   )
 }
 
+// CREATE TABLE users (
+//   id SERIAL PRIMARY KEY,
+//   email VARCHAR(100) NOT NULL UNIQUE,
+//   password VARCHAR(100) NOT NULL
+// );
+
+// create table todos (
+//     id SERIAL PRIMARY KEY,
+//     title TEXT NOT NULL,
+//     description TEXT,
+//     user_id INTEGER REFERENCES users(id),
+//     done BOOLEAN DEFAULT FALSE
+// );
+
 export default postgrestrial
